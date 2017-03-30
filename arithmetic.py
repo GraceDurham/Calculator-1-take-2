9ad0320406d782db5f5b727384590bbec3998474
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2017-03-30 12:46:38 -0700

    First commit

---
+++ arithmetic.py
@@ -0,0 +1,56 @@
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    
+    return num1 + num2
+
+print add(2, 2)
+
+def subtract(num1, num2):
+    """Return the difference of two numbers"""
+
+    return num1 - num2
+
+print subtract(10, 2)
+
+def multiply(num1, num2):
+    """Return the product of two numbers"""
+
+    return num1 * num2
+
+
+print multiply(5, 5)
+
+def divide(num1, num2):
+    """Return the quotient of two numbers as a float"""
+    
+    return num1/num2
+
+print divide(100, 10)
+
+def square(num):
+    """Return the square of a number"""
+    
+    return num * num 
+
+print square(10)
+
+def cube(num):
+    """Return the cube of a number"""
+
+    return num * num * num
+
+print cube(10)
+
+def power(num, exponent):
+    """Return num raised to the power of exponent"""
+    
+    return num ** exponent
+
+print power(10, 3)
+
+def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
+
+    return num1 % num2
+
+print mod(12, 2)
